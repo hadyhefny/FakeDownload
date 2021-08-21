a5fd967f95bc53be2c748a2d5b7f53474d403f0d
hadyhefny <hady.hefny@gmail.com>
2021-08-21 15:00:44 +0200

    update videos list in cache

--- app/src/main/java/com/hefny/hady/fakedownload/data/remote/responses/VideosListResponse.kt
+++ app/src/main/java/com/hefny/hady/fakedownload/data/remote/responses/VideosListResponse.kt
@@ -14,22 +14,22 @@ data class VideosListResponse(
         videoResponses.add(
             VideoItemResponse(
                 id = 2, type = "VIDEO",
-                url = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
+                url = "https://bestvpn.org/html5demos/assets/dizzy.mp4",
                 name = "Video 2"
             )
         )
         videoResponses.add(
             VideoItemResponse(
-                id = 3, type = "VIDEO",
-                url = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-                name = "Video 3"
+                id = 4, type = "VIDEO",
+                url = "https://storage.googleapis.com/exoplayer-test-media-1/mp4/frame-counter-one-hour.mp4",
+                name = "Video 4"
             )
         )
         videoResponses.add(
             VideoItemResponse(
-                id = 4, type = "VIDEO",
-                url = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-                name = "Video 4"
+                id = 6, type = "VIDEO",
+                url = "https://storage.googleapis.com/exoplayer-test-media-1/mp4/android-screens-10s.mp4",
+                name = "Video 6"
             )
         )
     }
